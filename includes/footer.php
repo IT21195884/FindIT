d58936148a5feeb0223d5f200867f1a514d7f83d
WolfRayet-102 <pattriciaruth@gmail.com>
2026-05-21 14:04:38 +0800

    test email file deleted and added cleaned the footer file

--- includes/footer.php
+++ includes/footer.php
@@ -5,6 +5,7 @@
     </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/findit/assets/js/main.js"></script>
+<script src="/assets/js/main.js"></script>
 </body>
 </html>
+ 
