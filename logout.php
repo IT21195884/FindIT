34ec4de3e6541c978bcb14c1ff89fd6f264a8a92
WolfRayet-102 <pattriciaruth@gmail.com>
2026-05-21 11:55:35 +0800

    logout bug fixed

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
-require_once 'includes/header.php';
+session_start();
+session_unset();
 session_destroy();
-header("Location: index.php");
+ 
+// Redirect to homepage
+header("Location: /index.php");
 exit();
 ?>
+ 
